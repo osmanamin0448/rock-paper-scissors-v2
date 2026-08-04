404fe4ecdd79f84af93c4f00776fac3ad7e326f1
osmanamin0448 <osmanamin0448@gmail.com>
2026-08-04 10:45:02 +0000

    Add comments to code structure

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Generate computer's choice using Math.random() between 0 and 3
 function getComputerChoice(){
   const computerChoice =  Math.floor(Math.random() * 3)
   if(computerChoice === 0){
@@ -11,14 +12,18 @@ function getComputerChoice(){
   }
 }
 
+// Take human choice as input
 function getHumanChoice(){
   const humanChoice = prompt("Enter your choice: ")
   return humanChoice.toLowerCase();
 }
 
+// Update human and computer Score
 let humanScore = 0;
 let computerScore = 0;
 
+
+//Play only a round of the game
 function playRound(computerChoice, humanChoice){
   if(humanChoice === "rock" && computerChoice ==="paper"){
     console.log("You lose! Paper beats Rock");
